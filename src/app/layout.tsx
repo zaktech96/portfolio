80e1b4960ec357bdb3b255861df00591ebc871b3
zaktech96 <zakariyesahid96@gmail.com>
2025-01-15 19:43:50 +0000

    hero section type effect fixed

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Zakariye | Portfolio',
+  description: 'Full Stack Developer Portfolio showcasing my projects and skills',
+  icons: {
+    icon: [
+      {
+        url: '/logo.svg', // We'll create this file
+        href: '/logo.svg',
+      },
+    ],
+  },
+}; 
